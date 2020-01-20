875157292d6e0e36e40872c078d962688084c7ec
Michael Wong <michael.k.wong@duke.edu>
2020-01-20 16:01:56 -0500

    Modified calculator module

--- calculator.py
+++ calculator.py
@@ -1,40 +1,57 @@
 # calculator.py
 
-def add(x, y):
-    z = x + y
-    print("{} + {} = {}".format(x, y, z))
+def add(num1, num2):
+    z = num1 + num2
+    print("{} + {} = {}".format(num1, num2, z))
+    symbol = "+"
+    return z, symbol
+
+def subtract(num1, num2):
+    z = num1 - num2
+    print("{} - {} = {}".format(num1, num2, z))
+    return z
+    
+def multiply(num1, num2):
+    z = num1 * num2
+    print("{} * {} = {}".format(num1, num2, z))
+    return z
+    
+def divide(num1, num2):
+    z = num1 / num2
+    print("{} / {} = {}".format(num1, num2, z))
     return z
 
-def subtract(x, y):
-    z = x - y
-    print("{} - {} = {}".format(x, y, z))
-    return z
-    
-def multiply(x, y):
-    z = x * y
-    print("{} * {} = {}".format(x, y, z))
-    return z
-    
 x = input("Enter a letter: ")
 print("You entered {}".format(x))
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+#print("You entered {} and {}".format(x))
+
 if x == "a":
-    d = add(56, 73)
-    if d > 100:
-        print("This number is too high.")
-if x == "b":
-    d = subtract(20, 3)
-if x == "c":
-    d = multiply(4, 10)
+    d = add(num1, num2)
 elif x == "s":
-    a = 20
-    b = -3
-    c = a - b
-    print("{} + {} = {}".format(a, b, c))
+    d = subtract(num1, num2)
 elif x == "m":
-    a = 3
-    b = -4
-    c = a * b
-    print("{} * {} = {}".format(a, b, c))
+    d = multiply(num1, num2)
+elif x == "d":
+    d = divide (num1, num2)
+    
+#elif x == "j":
+#    a = 20
+#    b = -3
+#    c = a - b
+#    print("{} + {} = {}".format(a, b, c))
+#elif x == "k":
+#    a = 3
+#    b = -4
+#    c = a * b
+#    print("{} * {} = {}".format(a, b, c))
 else:
     print("The {} command is not recognized.".format(x))
+
 print("Done")
+
+# input letter
+# input two numbers
+# use to call
